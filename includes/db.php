b6e3ab851212f5994a763c32a820f71e636cfaf6
Olonor <trofimmatveev00@gmail.com>
2021-12-09 14:21:17 +0900

    1

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,7 @@
 <?php
 require 'libs/rb-mysql.php';
 require 'config.php';
+$name = $config['db']['name'];
 
-try{
-    $db = new PDO("mysql:host=${$config['db']['host']};dbname=${$config['db']['name']}",
-        $config['db']['user'], $config['db']['password']);
-} catch(PDOException $e){
-    echo $e->getmessage();
-}
+R::setup( "mysql:host=${$config['db']['localhost']};dbname=$name",
+    $config['db']['user'], $config['db']['password']);
