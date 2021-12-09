69babcb10da28b87e10ac102a5a66bebe992e0bf
Olonor <trofimmatveev00@gmail.com>
2021-12-09 12:21:49 +0900

    error handling

--- db.php
+++ db.php
@@ -5,6 +5,7 @@ require 'config.php';
 try{
     $db = new PDO("mysql:host=${$config['db']['host']};dbname=${$config['db']['name']}",
         $config['db']['user'], $config['db']['password']);
+        $user = R::dispense('user');
 } catch(PDOException $e){
     echo $e->getmessage();
 }
